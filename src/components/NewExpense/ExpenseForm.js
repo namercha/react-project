23fc961614880fd6b3f371146ee833cee47ce1e0
nabil <merchant.nabil@gmail.com>
2022-01-28 21:16:43 -0600

    Adding a button for adding a new expense and displaying it conditionally

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -63,37 +63,40 @@ const ExpenseForm = (props) => {
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
-          <label>Title</label>
+          <label> Title </label>{' '}
           <input
             type="text"
             value={enteredTitle}
             onChange={titleChangeHandler}
-          />
-        </div>
+          />{' '}
+        </div>{' '}
         <div className="new-expense__control">
-          <label>Amount</label>
+          <label> Amount </label>{' '}
           <input
             type="number"
             min="0.01"
             step="0.01"
             value={enteredAmount}
             onChange={amountChangeHandler}
-          />
-        </div>
+          />{' '}
+        </div>{' '}
         <div className="new-expense__control">
-          <label>Date</label>
+          <label> Date </label>{' '}
           <input
             type="date"
             min="2019-01-01"
             max="2022-12-31"
             value={enteredDate}
             onChange={dateChangeHandler}
-          />
-        </div>
-      </div>
+          />{' '}
+        </div>{' '}
+      </div>{' '}
       <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
-      </div>
+        <button type="button" onClick={props.onCancel}>
+          Cancel{' '}
+        </button>{' '}
+        <button type="submit"> Add Expense </button>{' '}
+      </div>{' '}
     </form>
   );
 };
